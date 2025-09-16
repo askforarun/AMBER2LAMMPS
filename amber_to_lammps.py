d96378c412e8e42d22c512ae3ff38a21b5b10d63
askforarun <51930990+askforarun@users.noreply.github.com>
2025-09-16 07:03:03 +0100

    Update amber_to_lammps.py

--- amber_to_lammps.py
+++ amber_to_lammps.py
@@ -2,7 +2,7 @@
 import parmed as pmd
 import numpy as np
 
-#The main code that writes LAMMPS input file and data file for  amber
+#The main code that writes LAMMPS input file and data 
 def amber2lammps(ars):
     AmberParm=pmd.amber.AmberParm
     printBonds=pmd.tools.actions.printBonds
