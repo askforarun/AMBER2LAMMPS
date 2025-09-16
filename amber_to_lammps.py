f66143da61da8f28bc352afaac69d2c91020e09a
Arun <askforarun@gmail.com>
2025-09-16 05:51:16 +0100

    changes

--- amber_to_lammps.py
+++ amber_to_lammps.py
@@ -2,7 +2,7 @@
 import parmed as pmd
 import numpy as np
 
-#The main code that writes LAMMPS input file and data file for AMBER
+#The main code that writes LAMMPS input file and data file for AMBER force field
 def amber2lammps(ars):
     
     printBonds=pmd.tools.actions.printBonds
